8d812da07b129c94783401145a0576aec6ed0f4c
Jacob Buffington <jacob.e.buffington@gmail.com>
2023-09-15 12:52:41 -0400

    Added the James Bond introduction statement.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,6 @@ class Program
         string first_name = Console.ReadLine();
         Console.WriteLine("What is your last name?: ");
         string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
