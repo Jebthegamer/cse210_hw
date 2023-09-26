c16c45a785245a16d441cf45e1074cfb3f357ce3
Jacob Buffington <jacob.e.buffington@gmail.com>
2023-09-26 16:00:58 -0400

    Completed Resume Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Janitor";
+        job1._company = "Time Travel Inc.";
+        job1._endYear = 2050;
+        job1._startYear = 2060;
+        Job job2 = new Job();
+        job2._jobTitle = "Cabbage merchant";
+        job2._company = "Independently Run";
+        job2._startYear = 2020;
+        job2._endYear = 2020;
+        Resume myResume = new Resume();
+        myResume._name = "Jacob Buffington";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
     }
 }
