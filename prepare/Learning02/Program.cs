86cc7759ec747ab4e926151683bbc0cb59e00848
Jacob Buffington <jacob.e.buffington@gmail.com>
2023-09-26 16:47:53 -0400

    Used the recommended formatting for class definition.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,16 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1.JobTitle = "Janitor";
-        job1.Company = "Time Travel Inc.";
-        job1.EndYear = 2050;
-        job1.StartYear = 2060;
-        Job job2 = new Job();
-        job2.JobTitle = "Cabbage merchant";
-        job2.Company = "Independently Run";
-        job2.StartYear = 2020;
-        job2.EndYear = 2020;
+        Job job1 = new Job
+        {
+            JobTitle = "Janitor",
+            Company = "Time Travel Inc.",
+            EndYear = 2050,
+            StartYear = 2060
+        };
+        Job job2 = new Job
+        {
+            JobTitle = "Cabbage merchant",
+            Company = "Independently Run",
+            StartYear = 2020,
+            EndYear = 2020
+        };
         Resume myResume = new Resume();
         myResume.Name = "Jacob Buffington";
         myResume.Jobs.Add(job1);
