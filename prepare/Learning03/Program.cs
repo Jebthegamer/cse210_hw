30f83f6063254e9d80f00ba1f836a2a9ebfd269a
Jacob Buffington <jacob.e.buffington@gmail.com>
2023-10-03 16:57:08 -0400

    Added program logic comments

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Initialize variables
+
+        /* Start loop
+            Display the menu
+            Ask which option the user should do
+                1: Write an entry
+                    1A: Display the prompt
+                    1B: Ask for user input
+                        1AA: Store the date, prompt, and response in an entry
+                2:  Display the entries
+                    2A: Retrieve the journal's entry list
+                    2B: Print the list, including the prompt, date, and response.
+                        2AA: For loop, going through the entries in the entry list contained in Journal class
+                3: Load
+                    3A: Read a file
+                        3AA: Read each line, store as entries. Every other line will be a date/prompt then an entry
+                        3AB: While loop; or for lines in file store one line as the first part of an entry; then the next line as
+                            another part of the entry.
+                4: Save file
+                    4A: Print entries from journal onto the file
+                5: Quit
+                    5A: End while loop; close files (if needed), end program.
+        */
     }
 }
