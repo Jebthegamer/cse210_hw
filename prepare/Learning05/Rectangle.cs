4d425d0314a9be4f11fb102efe6ce3c9daf1b169
Jacob Buffington <jacob.e.buffington@gmail.com>
2023-11-07 16:55:35 -0500

    Completed the Shapes Activity.

---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+
+public class Rectangle : Shape
+{
+    private double _length { get; set; }
+    private double _width { get; set; }
+    public Rectangle(double length, double width, string color) :base (color)
+    {
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea()
+    {
+        double area = _length * _width;
+        return area;
+    } 
+}
